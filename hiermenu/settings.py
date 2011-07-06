a3adabc8176bee1c01583508bc37a8e157c2ae07
Adrien Lemaire <adrien.lemaire@aquasys.co.jp>
2011-07-06 19:34:05 +0000

    new context_processor menu, added to settings. A bit intrusive though

--- hiermenu/settings.py
+++ hiermenu/settings.py
@@ -1,18 +1,25 @@
 from django.conf import settings
+import django.conf.global_settings as DEFAULT_SETTINGS
+
 
 CACHE_TIMEOUT = getattr(settings, "HIERMENU_CACHE_TIMEOUT", 500)
 
 CACHE_KEY_PREFIX = getattr(settings, "HIERMENU_CACHE_KEY_PREFIX", 'M')
 
-UPLOAD_IMG_PATH = getattr(settings, "HIERMENU_UPLOAD_IMG_PATH", '/media/img/menu/')
+UPLOAD_IMG_PATH = getattr(settings, "HIERMENU_UPLOAD_IMG_PATH",
+    '/media/img/menu/')
 
 LOCATIONS = getattr(settings, "HIERMENU_LOCATIONS", (
         (1, 'None'),
-        (2, 'Top'), 
-        (3, 'Middle'), 
-        (4, 'Bottom'), 
-        (5, 'Left'), 
+        (2, 'Top'),
+        (3, 'Middle'),
+        (4, 'Bottom'),
+        (5, 'Left'),
         (6, 'Right'),
         (7, 'Other'),
-    )
+   )
 )
+
+settings.TEMPLATE_CONTEXT_PROCESSORS = getattr(settings,
+    "TEMPLATE_CONTEXT_PROCESSORS", DEFAULT_SETTINGS)
+settings.TEMPLATE_CONTEXT_PROCESSORS += ('hiermenu.context_processors.menu', )
