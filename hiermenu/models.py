dab8ad26d8dbd25116406d3e2772e2ebe7c35ee1
Adrien Lemaire <lemaire.adrien@gmail.com>
2011-12-12 17:30:32 +0900

    Added internationalization component to translated hiermenu fields name (ja files added)

--- hiermenu/models.py
+++ hiermenu/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from hiermenu import settings
 
@@ -24,45 +25,45 @@ class MenuManager(models.Manager):
 
 class Menu(models.Model):
     name = models.SlugField(max_length=100,
-        help_text='Menu name.', unique=True)
+        help_text=_('Menu name.'), unique=True)
     parent = models.ForeignKey("self", null=True, blank=True,
-        help_text='The parent MenuItem this item will be a subitem of.')
+        help_text=_('The parent MenuItem this item will be a subitem of.'))
     text = models.CharField(blank=True, max_length=50,
-        help_text='The text of the item.')
+        help_text=_('The text of the item.'))
     link = models.CharField(blank=True, max_length=255,
-        help_text='The url in which this item links to.')
+        help_text=_('The url in which this item links to.'))
     alt = models.CharField(blank=True, max_length=100,
-        help_text='The alternate text.')
+        help_text=_('The alternate text.'))
     order = models.PositiveIntegerField(default=1,
-        help_text='The order of the MenuItems.')
+        help_text=_('The order of the MenuItems.'))
     active_path_regex = models.CharField(blank=True, max_length=255,
-        help_text='A regular expression to identify this menu as active.')
+        help_text=_('A regular expression to identify this menu as active.'))
     location = models.PositiveIntegerField(choices=settings.LOCATIONS,
-        default=1, help_text='Location of menu.')
+        default=1, help_text=_('Location of menu.'))
     display = models.BooleanField(default=True,
-        help_text='Show this menu item.')
+        help_text=_('Show this menu item.'))
     cssclass = models.CharField(blank=True, max_length=100,
-        help_text='Extra css classes.')
+        help_text=_('Extra css classes.'))
     cssstyle = models.CharField(blank=True, max_length=100,
-        help_text='Extra css styles.')
+        help_text=_('Extra css styles.'))
     domid = models.CharField(blank=True, max_length=100,
-        help_text='A dom ID.')
+        help_text=_('A dom ID.'))
     active_cssclass = models.CharField(blank=True, max_length=100,
-        help_text='Extra css classes for active items.')
+        help_text=_('Extra css classes for active items.'))
     active_cssstyle = models.CharField(blank=True, max_length=100,
-        help_text='Extra css styles for active items.')
+        help_text=_('Extra css styles for active items.'))
     active_domid = models.CharField(blank=True, max_length=100,
-        help_text='A dom ID for active items.')
+        help_text=_('A dom ID for active items.'))
     img = models.ImageField(blank=True, null=True,
-        upload_to=settings.UPLOAD_IMG_PATH, help_text='A menu image.')
+        upload_to=settings.UPLOAD_IMG_PATH, help_text=_('A menu image.'))
     template_name = models.CharField(blank=True, max_length=100,
-        help_text='Template override.')
+        help_text=_('Template override.'))
     active_template_name = models.CharField(blank=True, max_length=100,
-        help_text='Acitve template override.')
+        help_text=_('Acitve template override.'))
     active = models.BooleanField(default=True,
-        help_text='Is this item active?')
-    groups = models.ManyToManyField(Group, blank=True,
-        help_text='Add one group or more if you want to make the menu private')
+        help_text=_('Is this item active?'))
+    groups = models.ManyToManyField(Group, blank=True, help_text=_('Add one '
+        'group or more if you want to make the menu private'))
 
     objects = MenuManager()
 
